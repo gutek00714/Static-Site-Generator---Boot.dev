caa04f842966956ade67e8cb1ce796db1bb3c211
gutek00714 <guciolaczkowski1@gmail.com>
2026-01-05 18:42:23 +0100

    Add LeafNode and tests

---
+++ src/test_htmlnode.py
@@ -0,0 +1,36 @@
+import unittest
+
+from htmlnode import HTMLNode, LeafNode
+
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode(tag="a", props={"href": "https://www.boot.dev/dashboard", "target": "_blank"})
+        expected = ' href="https://www.boot.dev/dashboard" target="_blank"'
+        self.assertEqual(node.props_to_html(), expected)
+
+    def test_props_to_html_none(self):
+        node = HTMLNode(tag="p")
+        self.assertEqual(node.props_to_html(), "")
+
+    def test_props_to_html_empty_dict(self):
+        node = HTMLNode(tag="div", props={})
+        self.assertEqual(node.props_to_html(), "")
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!",{"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
+            node.to_html()
