28361f38dee0d535a517413b5792fa8e77659269
gutek00714 <guciolaczkowski1@gmail.com>
2026-01-09 20:36:01 +0100

    Add generate_pages_recursive + update main

--- src/main.py
+++ src/main.py
@@ -21,10 +21,16 @@ def main():
 
     copy_static('./static', './public')
 
-    generate_page(
-        from_path="content/index.md",
-        template_path="template.html",
-        dest_path="public/index.html"
+    # generate_page(
+    #     from_path="content/index.md",
+    #     template_path="template.html",
+    #     dest_path="public/index.html"
+    # )
+
+    generate_pages_recursive(
+        dir_path_content="./content",
+        template_path="./template.html",
+        dest_dir_path="./public"
     )
 
 
@@ -207,6 +213,19 @@ def generate_page(from_path, template_path, dest_path):
 
     with open(dest_path, "w") as f:
         f.write(template_file)
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for item in os.listdir(dir_path_content):
+        new_path = os.path.join(dir_path_content, item)
+        dest_path = new_path.replace("content", "public")
+        if dest_path.endswith(".md"):
+            dest_path = dest_path[:-3] + ".html"
+        if os.path.isfile(new_path) and new_path.endswith(".md"):
+            generate_page(new_path, template_path, dest_path)
+        elif os.path.isdir(new_path):
+            generate_pages_recursive(new_path, template_path, dest_dir_path)
+
 
 
 
